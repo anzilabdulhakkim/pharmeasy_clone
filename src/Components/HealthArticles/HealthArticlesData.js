d4bb36cd0c7540e1d21aa52b497e106bf8b50180
anzilabdulhakkim <anzilabdulhakkim135@gmail.com>
2024-08-06 23:45:08 +0530

    modified file

--- src/Components/HealthArticles/HealthArticlesData.js
+++ src/Components/HealthArticles/HealthArticlesData.js
@@ -1,26 +1,26 @@
-export const HealthArticlesData=[
+export const HealthArticlesData = [
     {
-        img:"https://blog-images.pharmeasy.in/2022/09/29124727/19.jpg?dim=1440x0",
-        heading:"Benefits of Sudarshan Kriya and How to Do it By Dr. Ankit Sankhe"
+        img: "https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2024/08/01054707/celery-seed.webp?dim=1440x0",
+        heading: "Celery Seed: Health Benefits, Uses, Side Effects & More!"
     },
     {
-        img:"https://blog-images.pharmeasy.in/2022/09/29122712/12.jpg?dim=1440x0",
-        heading:"Benefits of Sukhasana (Easy Pose) and How to Do it By Dr. Himani Bisht"
+        img: "https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2024/07/01052931/seabuckthorn-oil.webp?dim=1440x0",
+        heading: "Sea Buckthorn Oil: Health Benefits, Uses, Side Effects & More!"
     },
     {
-        img:"https://blog-images.pharmeasy.in/2022/09/29121435/11.jpg?dim=1440x0",
-        heading:"Benefits of Bhastrika Pranayama and How to Do it By Dr. Ankit Sankhe"
+        img: "https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2024/06/31113153/cerasee-tea.webp?dim=1440x0",
+        heading: "Cerasee Tea: Health Benefits, Uses, Side Effects & More!"
     },
     {
-        img:"https://blog-images.pharmeasy.in/2022/09/29120043/9.jpg?dim=1440x0",
-        heading:"Benefits of Ustrasana (Camel Pose) and How to Do it By Dr. Ankit Sankhe"
+        img: "https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2024/07/31111417/what-foods-to-avoid-while-taking-metronidazole.webp?dim=1440x0",
+        heading: "What Foods to Avoid While Taking Metronidazole: A Comprehensive Guide"
     },
     {
-        img:"https://blog-images.pharmeasy.in/2022/09/29115156/20.jpg?dim=1440x0",
-        heading:"Benefits of Gomukhasana (Cow Face Pose) and How to Do it By Dr. Himani"
+        img: "https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2024/06/31111315/855.jpg?dim=1440x0",
+        heading: "Windburn: Reasons, Remedies, and Precautionary Measures Backed by Science"
     },
     {
-        img:"https://blog-images.pharmeasy.in/2022/09/28202427/10.jpg?dim=1440x0",
-        heading:"Benefits of Bhramari Pranayama and How to Do it By Dr. Himani Bisht"
+        img: "https://blog-images-1.pharmeasy.in/blog/production/wp-content/uploads/2024/07/31105747/sassafras-tea.webp?dim=1440x0",
+        heading: "Sassafras Tea: Health Benefits and How it Can Help You"
     }
-]
+];
